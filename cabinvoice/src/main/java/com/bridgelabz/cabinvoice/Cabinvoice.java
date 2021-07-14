7bac07042b08862fbfd920111066c3acf2799496
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-07-14 23:52:32 +0530

    [Pradip] ADDED:Calculate Total Fare Cost

---
+++ cabinvoice/src/main/java/com/bridgelabz/cabinvoice/Cabinvoice.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.cabinvoice;
+public class Cabinvoice
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome To CabInvoiceproblem" );
+    }
+}
