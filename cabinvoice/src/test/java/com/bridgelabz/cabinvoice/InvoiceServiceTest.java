6b5dd0147dc9418be47c8d44b40e3565f17a824f
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-07-15 00:25:29 +0530

    Refactored:Removed if else and used at the rate before in Testing 8f7ef69

--- cabinvoice/src/test/java/com/bridgelabz/cabinvoice/InvoiceServiceTest.java
+++ cabinvoice/src/test/java/com/bridgelabz/cabinvoice/InvoiceServiceTest.java
@@ -1,11 +1,18 @@
 package com.bridgelabz.cabinvoice;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
+    CabInvoiceGenerator cabInvoiceGenerator = null;
+
+    @Before
+    public void setUp() throws Exception {
+        cabInvoiceGenerator = new CabInvoiceGenerator();
+    }
+
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare(){
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = cabInvoiceGenerator.calculateFare(distance, time);
@@ -14,10 +21,18 @@ public class InvoiceServiceTest {
 
     @Test
     public void givenLessDistanceOrTime_ShouldReturnMinFare(){
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double fare = cabInvoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare(){
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double fare = cabInvoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30, fare,0.0);
     }
 }
